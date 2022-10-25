cc9055e56b1b6c6181a04360fd83d7d30f7cfd54
BaDxKaRMa <bad87karma@gmail.com>
2022-10-24 22:50:08 -0400

    logger edits

--- my_logger/logger.py
+++ my_logger/logger.py
@@ -28,8 +28,8 @@ if __name__ == "__main__":
     # Remove all handlers
     logger.remove()
     # Set loguru format
-    fmt = "<green>{time:YYYY-MM-DD hh:mm:ss A}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{" \
-          "function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level> "
+    fmt = '<green>{time:YYYY-MM-DD hh:mm:ss A}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{' \
+          'function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level> '
 
     if args.debug:
         logger.add(sys.stderr, format=fmt, level="DEBUG")
