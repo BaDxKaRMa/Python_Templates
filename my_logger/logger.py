600d98383de3730f44f38f1ed4b636e60c6db8aa
BaDxKaRMa <bad87karma@gmail.com>
2022-10-25 00:17:58 -0400

    updated format for time color

--- my_logger/logger.py
+++ my_logger/logger.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     # Remove all handlers
     logger.remove()
     # Set loguru format
-    fmt = '<green>{time:YYYY-MM-DD hh:mm:ss A}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{' \
+    fmt = '<level>{time:YYYY-MM-DD hh:mm:ss A}</level> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{' \
           'function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level> '
 
     if args.debug:
